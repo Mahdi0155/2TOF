c39bc4027f6ba056379d69550eb02fe3592e9cbe
Mahdi0155 <t.mehdti21@gmail.com>
2025-04-28 18:05:15 +0330

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+API_TOKEN = 'YOUR_API_TOKEN_HERE'
+
+DB_USER = 'postgres'
+DB_PASS = 'your_password'
+DB_HOST = 'localhost'
+DB_PORT = '5432'
+DB_NAME = 'your_database'
+
+FAKE_NAMES = [
+    "Parsa", "AmirAli", "Arad", "Arman", "Sina",
+    "Yasaman", "Haniyeh", "Niloofar", "Elina", "Diana"
+]
